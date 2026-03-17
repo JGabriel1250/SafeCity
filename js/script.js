1cc39395524c38b67b495f1fd43abcca4fe15c68
José Gabriel <gabrijuvis@gmail.com>
2026-03-17 01:17:36 +0000

    Adiciona layout completo, mapa Esri e filtros de busca
    - HTML estruturado com header, filtros, mapa e footer
    - CSS com gradiente, responsividade e estilo dos filtros
    - Mapa interativo com Leaflet e tile Esri
    - Corrige bug dos quadrados e erro de digitação no click

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const map = L.map('map').setView([-15.793889, -47.882778], 4)
+
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+    attribution: '&copy; Esri'
+}).addTo(map)
+
+
+// Isso resolve o problema dos quadrados quebrados
+setTimeout(() => {
+    map.invalidateSize()
+}, 300)
+
+// Pega o botão de buscar do HTM
+const botaoBuscar = document.getElementById('buscar')
+
+// Pega os selects e o input do HTML
+const selectEstado = document.getElementById('estado')
+const selectCidade = document.getElementById('cidade')
+const inputBairro = document.getElementById('bairro')
+
+// Quando o usuário clicar no botão buscar, executa essa função
+botaoBuscar.addEventListener("click", function() {
+
+    // Pega o texto que o usuário escolheu/digitou
+    const estadoEscolhido = selectEstado.value
+    const cidadeEscolhida = selectCidade.value
+    const bairroDigitado = inputBairro.value
+
+    // Avisa no console o que foi escolhido (útil para testar)
+    console.log('Estado:', estadoEscolhido)
+    console.log('Cidade:', cidadeEscolhida)
+    console.log('Bairro:', bairroDigitado)
+})
